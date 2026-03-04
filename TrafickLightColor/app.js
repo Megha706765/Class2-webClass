84d5e99ae1977c67f73769645d1fb99125d1c8e0
Megha706765 <meghathakur2003123@gmail.com>
2026-03-04 22:45:26 +0530

    ifStatement

---
+++ TrafickLightColor/app.js
@@ -0,0 +1,11 @@
+let color = "red";
+//Traffic light  System
+if(color === "red"){
+    console.log("Stop! light color is red.");
+}
+if(color === "green"){
+    console.log("go light color is green.");
+}
+if(color === "yellow"){
+    console.log("slow down light color is yellow.");
+}
