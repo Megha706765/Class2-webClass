1572453c7e84c82a3a82d2fbf226395c148fe3b0
Megha706765 <meghathakur2003123@gmail.com>
2026-03-04 21:34:01 +0530

    Update index.js

--- 2026JS CODE/index.js
+++ 2026JS CODE/index.js
@@ -1,3 +1,8 @@
+console.log("Enter the 1 to 6 number are print :")
 for(let i=1; i<=6; i++){
     console.log(i+" ");
+}
+console.log("Print reverse number :");
+for(let j=6; j>=1; j--){
+    console.log(j+" ");
 }
