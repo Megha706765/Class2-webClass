42baa5496151af90db1aef600519d28a7281fe70
Megha706765 <meghathakur2003123@gmail.com>
2026-03-06 10:07:46 +0530

    Create Alert & Prompt.js

---
+++ creatCalculationToPopconPrices/Alert & Prompt.js
@@ -0,0 +1,22 @@
+//ALERT MESSAGE
+
+alert("something is wrong!");
+console.log("This is a simple log");
+console.error("This is an error msg");
+console.warn("this is a warning msg");
+console.log("Hello Megha");
+
+
+//Prompt MESSAGE
+let firstName = prompt( "enter your name : ");
+console.log("name is : " + firstName);
+
+let lastName = prompt("enter the last name : ");
+console.log("last name is : " + lastName);
+
+let roll = prompt("enter your rollnumber : ");
+console.log("Roll number is : " + roll);
+
+// concanitation 
+
+console.log(firstName,"",lastName);
