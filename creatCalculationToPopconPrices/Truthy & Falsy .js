43f44bf544002343e0b69c86905be9f44ccaf89c
Megha706765 <meghathakur2003123@gmail.com>
2026-03-06 09:19:55 +0530

    Update Truthy & Falsy .js

---
+++ creatCalculationToPopconPrices/Truthy & Falsy .js
@@ -0,0 +1,73 @@
+if(1){
+    console.log("It has true value");
+}else{
+    console.log("it has false value");
+}
+
+
+if(''){
+    console.log("It has true value");
+}else{
+    console.log("it has false value");
+}
+
+
+if("apnacollege"){
+    console.log("It has true value");
+}else{
+    console.log("it has false value");
+}
+
+
+if(null){
+    console.log("It has true value");
+}else{
+    console.log("it has false value");
+}
+
+
+if(undefined){
+    console.log("It has true value");
+}else{
+    console.log("it has false value");
+}
+
+
+if("megha"){
+    console.log("It has true value");
+}else{
+    console.log("it has false value");
+}
+
+if(NaN){
+    console.log("It has true value");
+}else{
+    console.log("it has false value");
+}
+
+
+if(""){
+    console.log("It has true value");
+}else{
+    console.log("it has false value");
+}
+
+
+if(" "){
+    console.log("It has true value");
+}else{
+    console.log("it has false value");
+}
+
+
+if(0){
+    console.log("It has true value");
+}else{
+    console.log("it has false value");
+}
+
+if(-0){
+    console.log("It has true value");
+}else{
+    console.log("it has false value");
+}
